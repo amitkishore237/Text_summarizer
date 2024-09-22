7fe1eedd512277728c84d39163faec79abe878a8
Amit Kishore <amitkishore@Amits-MacBook-Air-2.local>
2024-09-22 16:01:22 +0530

    evaluationpipeline

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from src.text_summarizer.pipeline.stage_02_data_validation import DataValidation
 from src.text_summarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 from src.text_summarizer.pipeline.stage_04_model_trainer import ModelTrainingPipeline
 
+from src.text_summarizer.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
 
 
 STAGE_NAME="Data Ingestion Pipeline stage_01"
@@ -44,6 +45,17 @@ STAGE_NAME="Model Training Pipeline stage_04"
 try:
     logger.info(f"Starting {STAGE_NAME}...")
     data_validation = ModelTrainingPipeline()
+    data_validation.main()
+    logger.info(f"{STAGE_NAME} completed successfully.")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME="Model evaluation Pipeline stage_05"
+
+try:
+    logger.info(f"Starting {STAGE_NAME}...")
+    data_validation = ModelEvaluationTrainingPipeline()
     data_validation.main()
     logger.info(f"{STAGE_NAME} completed successfully.")
 except Exception as e:
