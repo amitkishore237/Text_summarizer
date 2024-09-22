860c06cf23f991cde008cb24413d74f98765c3dc
Amit Kishore <amitkishore@Amits-MacBook-Air-2.local>
2024-09-22 15:01:49 +0530

    validation steps

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.text_summarizer.logging import logger
 from src.text_summarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from src.text_summarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
 STAGE_NAME="Data Ingestion Pipeline stage_01"
 
@@ -12,5 +13,14 @@ except Exception as e:
     logger.exception(e)
     raise e
 
+STAGE_NAME="Data validation Pipeline stage_02"
 
+try:
+    logger.info(f"Starting {STAGE_NAME}...")
+    data_validation = DataValidationTrainingPipeline()
+    data_validation.main()
+    logger.info(f"{STAGE_NAME} completed successfully.")
+except Exception as e:
+    logger.exception(e)
+    raise e
 
